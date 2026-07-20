a4a545467c6e713fa8b5c71e15dd432d2e7554e7
Mongkol88 <flucktv25@gmail.com>
2026-07-20 16:47:24 +0700

    fix(week-02): update base path in vite.config.js to relative

--- labs/week-02/source/vite.config.js
+++ labs/week-02/source/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 const repositoryName = "engse203-lab02-68543210039-2";
 
 export default defineConfig({
-  base: `/${repositoryName}/labs/week-02/`,
+  base: './',
   build: {
     outDir: "docs",
     emptyOutDir: true,
